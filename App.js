f38f20ef65604a1b84422059d80f30ca09f47d4b
MundadaM1704 <83415791+MundadaM1704@users.noreply.github.com>
2023-04-13 14:26:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import React, {Component} from "react";
+import Main from "./screens/Main";
+
+export default function App() {
+  render(){
+    return(
+      <Main/>
+    )
+  }
+}
+
